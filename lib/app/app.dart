4ae8fa0cf217ff5b72f028b8d72d1354808c1a27
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-01-11 15:22:15 +0100

    added locator

---
+++ lib/app/app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../cores/constants/color.dart';
+import '../cores/navigator/app_router.dart';
+import '../cores/navigator/router_generator.dart';
+// import '../features/auth/views/intro_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, Widget? child) {
+        return MaterialApp(
+          title: 'Jolobbi',
+          theme: ThemeData(
+            errorColor: kcErrorColor,
+            scaffoldBackgroundColor: kcBackground,
+            primaryColor: kcPrimaryColor,
+            useMaterial3: true,
+          ),
+          // initialRoute: IntroScreen.route,
+          onGenerateRoute: RouteGenerator.generateRoute,
+          navigatorKey: AppRouter.instance.navigatorKey,
+        );
+      },
+    );
+  }
+}
---
+++ lib/app/locator.dart
@@ -0,0 +1,11 @@
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setUpLocator() {
+  ///--------------- Authentication --------------------//
+  // Repos
+  // getIt.registerLazySingleton<AuthRepository>(() => AuthRepositoryImp());
+
+  // Bloc
+}
