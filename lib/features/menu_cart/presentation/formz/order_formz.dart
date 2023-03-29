630360d48e160af2e6f190c750fd8c1c64c15290
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-03-29 22:22:38 +0100

    feat: update

---
+++ lib/features/menu_cart/presentation/formz/order_formz.dart
@@ -0,0 +1,20 @@
+// import 'package:equatable/equatable.dart';
+
+// import '../../../profile/domain/entities/address_entity.dart';
+// import '../../../profile/domain/entities/user_details_entity.dart';
+// import 'cart_item_formz.dart';
+
+// class OrderFormzModel extends Equatable {
+//   final List<CartItemFormz> items;
+//   final AddressEntity address;
+//   final UserDetailsEntity userDetails;
+//   final 
+
+//   bool hasRated = false;
+//   List<String> get vendors => items.map((e) => e.shopId.value).toSet().toList();
+//   List<String> get vendorsNames => items.map((e) => e.shopName.value).toList();
+
+//   Map<String, dynamic> toMap() {
+//     return {};
+//   }
+// }
--- lib/features/menu_cart/presentation/pages/cart_view.dart
+++ lib/features/menu_cart/presentation/pages/cart_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../app/locator.dart';
 import '../../../../cores/navigator/navigator.dart';
 import '../../../../cores/utils/utils.dart';
 import '../../../../cores/components/components.dart';
+import '../cubit/cart_items_cubit.dart';
 import '../widgets/cart_widget/food_cart_header_widget.dart';
 import '../widgets/cart_widget/food_cart_list_widget.dart';
 import 'confirm_food_order_view.dart';
@@ -10,6 +13,9 @@ class MenuCartView extends StatelessWidget {
   static const String route = "/menu_cart";
 
   const MenuCartView({super.key});
+
+  static final CartItemCubit _cartItemCubit =
+      SetUpLocators.getIt<CartItemCubit>();
 
   @override
   Widget build(BuildContext context) {
@@ -20,11 +26,19 @@ class MenuCartView extends StatelessWidget {
           verticalSpace(20),
           const FoodCartListWidget(),
           verticalSpace(40),
-          Button(
-            text: "Checkout",
-            onTap: () => AppRouter.instance.navigateTo(
-              ConfirmFoodOrderView.route,
-            ),
+          BlocBuilder<CartItemCubit, CartItemsList>(
+            bloc: _cartItemCubit,
+            builder: (context, state) {
+              return Visibility(
+                visible: state.cartItems.isNotEmpty,
+                child: Button(
+                  text: "Checkout",
+                  onTap: () => AppRouter.instance.navigateTo(
+                    ConfirmFoodOrderView.route,
+                  ),
+                ),
+              );
+            },
           ),
           verticalSpace(20),
         ],
